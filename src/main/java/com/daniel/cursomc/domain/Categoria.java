d664e59cf7ee63e57b5389131a86f00af0e6cf7a
Daniel Soares <danielsf850@gmail.com>
2021-02-18 12:22:41 -0300

    testando classe de domínio

---
+++ src/main/java/com/daniel/cursomc/domain/Categoria.java
@@ -0,0 +1,41 @@
+package com.daniel.cursomc.domain;
+
+import java.io.Serializable;
+
+public class Categoria implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	private String nome;
+	
+	public Categoria() {
+	}
+
+	public Categoria(Integer id, String nome) {
+		this.id = id;
+		this.nome = nome;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public String toString() {
+		return "Categoria [id=" + id + ", nome=" + nome + "]";
+	}
+	
+}//class Categoria 
